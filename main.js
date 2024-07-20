7726181a0aa3b6a284425183e9494fed9f201b85
scribeNcode <deyeye99@gmail.com>
2024-07-20 02:08:04 +0100

    add landing page airbnbViews with pricing details

--- main.js
+++ main.js
@@ -13,9 +13,34 @@ let airbnbPoductsGrid = document.querySelector(".airbnbsGrid")
 .then((data)=>{
  const display = data.map((datum)=>{
         return `
-        <>
+        <div>
+        <div class = "apartmentImageDiv">
+        <img src= ${datum.img} alt="" srcset="">
+        </div>
+        <div class="airbnbInfo">
+        <div class="locationRatingDiv">
+        <p>${datum.apartmentName}</p>
+        <div class="ratingLevel">
+                <object
+              data="./assets/icons/ratingstar.svg"
+              width=""
+              type="image/svg+xml"
+            ></object>
+            <p>${datum.rating}</p>
+        </div>
+
+        </div>
+        <p class="apartmentView">${datum.view}</p>
+        <p class="apartmentView">${datum.date}</p>
+        <p class="apartmentView">${datum.price}</p>
+    
+        </div>
+        
+        </div>
         `
     })
+
+    airbnbPoductsGrid.innerHTML = display
 })
 }
 
